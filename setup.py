27b4c758606f93f4a41179f2e72efef0096af64b
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-09-15 15:59:12 -0500

    added additional information to packet + dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from distutils.core import setup
 
-setup(name='waggle',
-      version='0.1',
-      description='',
-      packages=['waggle',
-                'waggle.platform'])
+setup(
+    name='waggle',
+    version='0.1',
+    description='Python Waggle Module',
+    url='https://github.com/waggle-sensor/pywaggle',
+    install_requires=[
+        'pika',
+    ],
+    packages=[
+        'waggle',
+        'waggle.platform'
+    ],
+)
