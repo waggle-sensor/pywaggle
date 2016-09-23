eb230b70fcbbfbecd86c00e70a2590ebe52b35f3
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-09-23 09:05:11 -0500

    added macaddr detect

--- waggle/platform/__init__.py
+++ waggle/platform/__init__.py
@@ -1,16 +1,21 @@
 import re
+import subprocess
 
 
-cache = dict()
+def cached(func):
+    def cachedfunc():
+        if not hasattr(cachedfunc, 'value'):
+            cachedfunc.value = func()
+        return cachedfunc.value
+    return cachedfunc
 
 
+def first(p, s):
+    return next(filter(p, s))
+
+
+@cached
 def hardware():
-    if 'hardware' not in cache:
-        cache['hardware'] = detect_hardware()
-    return cache['hardware']
-
-
-def detect_hardware():
     with open('/proc/cpuinfo') as f:
         match = re.search('ODROID-?(.*)', f.read())
         model = match.group(1)
@@ -19,3 +24,10 @@ def detect_hardware():
         if model.startswith('XU'):
             return 'XU4'
         raise RuntimeError('Unknown hardware.')
+
+
+@cached
+def macaddr():
+    output = subprocess.check_output(['ip', 'link'])
+    addrs = re.findall('link/ether (\S+)', output)
+    return first(lambda m: m.startswith('00:1e:06'), addrs).replace(':', '')
