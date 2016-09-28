4c85b421c3dc4f540581f5d0d5523b9f5abdee29
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-09-28 11:46:46 -0500

    streamlined register / run_standalone / split up backend

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='waggle',
-    version='0.3.12',
+    version='0.3.13',
     description='Python Waggle Module',
     url='https://github.com/waggle-sensor/pywaggle',
     install_requires=[
--- waggle/pipeline.py
+++ waggle/pipeline.py
@@ -8,6 +8,109 @@ import waggle.platform
 
 # TODO Set this up to use different backends.
 
+
+class PluginBackend(object):
+
+    def connect(self):
+        pass
+
+    def send(self, sensor, data):
+        pass
+
+
+class PluginManagerBackend(PluginBackend):
+
+    def __init__(self, queue):
+        self.queue = queue
+
+    def connect(self):
+        pass
+
+    def send(self, sensor, data):
+        now = time.time()
+        timestamp_epoch = int(now * 1000)
+        timestamp_utc = int(now)
+        timestamp_date = time.strftime('%Y-%m-%d', time.gmtime(timestamp_utc))
+
+        self.queue.put([
+            str(timestamp_date),
+            self.plugin_name,
+            self.plugin_version,
+            '',
+            timestamp_epoch,
+            sensor,
+            '',
+            ['data:{}'.format(data)],
+        ])
+
+
+class RabbitMQBackend(PluginBackend):
+
+    def __init__(self, host='localhost'):
+        self.host = host
+
+        # NOTE I strongly dislike this and think it should be handled in a more
+        # weakly coupled way. This is worth creating a better design for.
+        self.node_id = waggle.platform.macaddr()
+
+    def connect(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(self.host))
+
+        self.channel = self.connection.channel()
+
+        self.channel.exchange_declare('sensor-data',
+                                      exchange_type='fanout',
+                                      durable=True)
+
+        self.channel.queue_declare(queue='sensor-data',
+                                   durable=True)
+
+        self.channel.queue_bind(exchange='sensor-data',
+                                queue='sensor-data')
+
+    def send(self, sensor, data):
+        if isinstance(data, int):
+            # consider packing certain types to save space.
+            # can also consider automatically applying zlib
+            # if it makes sense on the data. it'd be even
+            # better if we could take advantage of protobufs
+            # or some standard, open encoding technique.
+            # data = struct.pack('>i', data)
+            data = str(data).encode()
+            datatype = 'i'
+        elif isinstance(data, float):
+            # data = struct.pack('>f', data)
+            data = str(data).encode()
+            datatype = 'f'
+        elif isinstance(data, str):
+            data = data.encode()
+            datatype = 's'
+        elif isinstance(data, bytearray):
+            data = bytes(data)
+            datatype = 'b'
+        elif isinstance(data, bytes):
+            datatype = 'b'
+        else:
+            raise ValueError('unsupported data type')
+
+        properties = pika.BasicProperties(
+            delivery_mode=2,  # persist message
+            type=datatype,
+            timestamp=int(time.time() * 1000),
+            headers={
+                'node': self.node_id,
+                'plugin': [self.plugin_name, self.plugin_version, ''],
+                'key': sensor,
+            }
+        )
+
+        self.channel.basic_publish(properties=properties,
+                                   exchange='sensor-data',
+                                   routing_key='',
+                                   body=data)
+
+
 class Plugin(object):
 
     def __init__(self, name, man, outqueue):
@@ -21,24 +124,8 @@ class Plugin(object):
         self.man = man
         self.outqueue = outqueue
 
-        # self.connection = pika.BlockingConnection(
-        #     pika.ConnectionParameters('localhost'))
-        #
-        # self.channel = self.connection.channel()
-        #
-        # self.channel.exchange_declare('sensor-data',
-        #                               exchange_type='fanout',
-        #                               durable=True)
-        #
-        # self.channel.queue_declare(queue='sensor-data',
-        #                            durable=True)
-        #
-        # self.channel.queue_bind(exchange='sensor-data',
-        #                         queue='sensor-data')
-
-        # NOTE I strongly dislike this and think it should be handled in a more
-        # weakly coupled way. This is worth creating a better design for.
-        self.node_id = waggle.platform.macaddr()
+        # self.backend = RabbitMQBackend('localhost')
+        # self.backend.connect()
 
     def send(self, sensor, data):
         assert isinstance(sensor, str)
@@ -60,49 +147,23 @@ class Plugin(object):
         ]
 
         self.outqueue.put(message_data)
-
-        # if isinstance(data, int):
-        #     # consider packing certain types to save space.
-        #     # can also consider automatically applying zlib
-        #     # if it makes sense on the data. it'd be even
-        #     # better if we could take advantage of protobufs
-        #     # or some standard, open encoding technique.
-        #     # data = struct.pack('>i', data)
-        #     data = str(data).encode()
-        #     datatype = 'i'
-        # elif isinstance(data, float):
-        #     # data = struct.pack('>f', data)
-        #     data = str(data).encode()
-        #     datatype = 'f'
-        # elif isinstance(data, str):
-        #     data = data.encode()
-        #     datatype = 's'
-        # elif isinstance(data, bytearray):
-        #     data = bytes(data)
-        #     datatype = 'b'
-        # elif isinstance(data, bytes):
-        #     datatype = 'b'
-        # else:
-        #     raise ValueError('unsupported data type')
-        #
-        # properties = pika.BasicProperties(
-        #     delivery_mode=2,  # persist message
-        #     type=datatype,
-        #     timestamp=int(time.time() * 1000),
-        #     headers={
-        #         'node': self.node_id,
-        #         'plugin': [self.plugin_name, self.plugin_version, ''],
-        #         'key': sensor,
-        #     }
-        # )
-        #
-        # self.channel.basic_publish(properties=properties,
-        #                            exchange='sensor-data',
-        #                            routing_key='',
-        #                            body=data)
+        # self.backend.send(sensor, data)
 
     def run(self):
         raise NotImplemented('Plugin must define run method.')
+
+    @classmethod
+    def register(cls, name, man, mailbox_outgoing):
+        cls(name, man, mailbox_outgoing).run()
+
+    @classmethod
+    def run_standalone(cls, callback):
+        q = Queue()
+        p = Process(target=cls.register, args=(cls.plugin_name, {}, q))
+        p.start()
+
+        while True:
+            callback(q.get())
 
 
 class Worker(object):
@@ -123,7 +184,7 @@ class Worker(object):
 
         self.channel.queue_declare(queue=self.queue_name)
 
-        self.channel.queue_bind(exchange='x-plugins-in',
+        self.channel.queue_bind(exchange='plugins-in',
                                 queue=self.queue_name,
                                 routing_key=self.routing_key)
 
@@ -132,12 +193,14 @@ class Worker(object):
 
     def put_message(self, headers, payload):
         payload.update({
+            # this should still be the internal node id. translation to "nice"
+            # id will happen in the plenario push plugin.
             'node_id': '00A',
             'node_config': '123abc',
             'datetime': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f'),
         })
 
-        self.channel.basic_publish(exchange='x-plugins-out',
+        self.channel.basic_publish(exchange='plugins-out',
                                    routing_key='',
                                    # routing_key='envsense.2',
                                    body=json.dumps(payload))
@@ -150,16 +213,3 @@ class Worker(object):
 
         self.channel.basic_consume(callback, queue=self.queue_name, no_ack=True)
         self.channel.start_consuming()
-
-
-def run_standalone(plugin, callback):
-
-    def register(name, man, mailbox_outgoing):
-        plugin(name, man, mailbox_outgoing).run()
-
-    q = Queue()
-    p = Process(target=register, args=(plugin.plugin_name, {}, q))
-    p.start()
-
-    while True:
-        callback(q.get())
