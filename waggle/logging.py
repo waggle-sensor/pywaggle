08f03acd8f7e7681c5300f382aa97b3c3e78f7cb
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-10-11 17:20:54 -0500

    added beehive logging handler

--- waggle/logging.py
+++ waggle/logging.py
@@ -1,22 +1,53 @@
+import logging
 import pika
 import time
 
 
-connection = pika.BlockingConnection(pika.URLParameters('amqp://localhost'))
+class BeehiveHandler(logging.Handler):
 
-channel = connection.channel()
+    def __init__(self, url='amqp://localhost', exchange='logs'):
+        logging.Handler.__init__(self)
 
-channel.exchange_declare('logs',
-                         exchange_type='topic',
-                         durable=True)
+        self.connection = pika.BlockingConnection(pika.URLParameters(url))
 
+        self.channel = self.connection.channel()
 
-def log(topic, body):
-    properties = pika.BasicProperties(
-        timestamp=int(time.time() * 1000)
-    )
+        self.channel.exchange_declare(exchange=exchange,
+                                      exchange_type='topic')
 
-    channel.basic_publish(properties=properties,
-                          exchange='logs',
-                          routing_key=topic,
-                          body=body)
+        self.exchange = exchange
+
+    def emit(self, record):
+        params = record.__dict__
+        routing_key = '{}.{}'.format(params['name'].lower(),
+                                     params['levelname'].lower())
+
+        body = self.format(record)
+
+        properties = pika.BasicProperties(timestamp=int(time.time() * 1000))
+
+        self.channel.basic_publish(properties=properties,
+                                   exchange=self.exchange,
+                                   routing_key=routing_key,
+                                   body=body)
+
+#
+#
+# connection = pika.BlockingConnection(pika.URLParameters('amqp://localhost'))
+#
+# channel = connection.channel()
+#
+# channel.exchange_declare('logs',
+#                          exchange_type='topic',
+#                          durable=True)
+#
+#
+# def log(topic, body):
+#     properties = pika.BasicProperties(
+#         timestamp=int(time.time() * 1000)
+#     )
+#
+#     channel.basic_publish(properties=properties,
+#                           exchange='logs',
+#                           routing_key=topic,
+#                           body=body)
