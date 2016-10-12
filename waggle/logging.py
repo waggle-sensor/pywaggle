98a9854ca42d7fbe1c03edb866cc708c295c3666
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-10-11 21:16:00 -0500

    added basic closed connection handler to logging handler

--- waggle/logging.py
+++ waggle/logging.py
@@ -8,14 +8,9 @@ class BeehiveHandler(logging.Handler):
     def __init__(self, url='amqp://localhost', queue='logs'):
         logging.Handler.__init__(self)
 
-        self.connection = pika.BlockingConnection(pika.URLParameters(url))
-
-        self.channel = self.connection.channel()
-
-        self.channel.queue_declare(queue=queue,
-                                   durable=True)
-
+        self.url = url
         self.queue = queue
+        self.connect()
 
     def emit(self, record):
         params = record.__dict__
@@ -34,7 +29,29 @@ class BeehiveHandler(logging.Handler):
             headers=headers
         )
 
-        self.channel.basic_publish(properties=properties,
-                                   exchange='',
-                                   routing_key=self.queue,
-                                   body=body)
+        self.publish(properties, body)
+
+    def connect(self):
+        parameters = pika.URLParameters(self.url)
+
+        while True:
+            try:
+                self.connection = pika.BlockingConnection(parameters)
+                self.channel = self.connection.channel()
+                self.channel.queue_declare(queue=self.queue, durable=True)
+            except pika.exceptions.ConnectionClosed:
+                pass
+            else:
+                break
+
+    def publish(self, properties, body):
+        while True:
+            try:
+                self.channel.basic_publish(properties=properties,
+                                           exchange='',
+                                           routing_key=self.queue,
+                                           body=body)
+            except pika.exceptions.ConnectionClosed:
+                self.connect()
+            else:
+                break
