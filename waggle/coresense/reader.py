6b83217ed58713ae327dad48d09016629883b262
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-02-06 11:37:31 -0600

    added more logging and robustness to coresense reader

--- waggle/coresense/reader.py
+++ waggle/coresense/reader.py
@@ -1,6 +1,7 @@
 from waggle.transport import TimedTransport
 from waggle.coresense.utils import decode_frame
 from binascii import hexlify
+import sys
 
 
 class CoresenseReader(object):
@@ -14,8 +15,16 @@ class CoresenseReader(object):
     def __iter__(self):
         for packet in self.transport:
             try:
-                data = decode_frame(packet)
-            except Exception as e:
-                print('Could not decode data: data={} exc={}'.format(hexlify(packet), e))
+                start = packet.index(0xAA)
+                end = packet.rindex(0x55) + 1
+
+                assert start < end
+
+                if start != 0 and end != len(packet):
+                    print('WARN unexpected packet padding: {}'.format(hexlify(packet)), file=sys.stderr)
+
+                data = decode_frame(packet[start:end])
+            except Exception as exc:
+                print('ERROR could not decode packet: {} <{}>'.format(hexlify(packet), exc), file=sys.stderr)
             else:
                 yield data
