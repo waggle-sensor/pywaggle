3402c0a7f1edf5a735ebb9a3bb7ae0ae1db24a8b
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-06-19 16:52:31 -0500

    Added ClientConfig and MessageClient.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='waggle',
-    version='0.13.2',
+    version='0.14.0',
     description='Python Waggle Module',
     url='https://github.com/waggle-sensor/pywaggle',
     install_requires=[
