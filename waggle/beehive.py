4a9109b460fccaac9daffbaec1a7affeed031f02
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-06-21 10:50:56 -0500

    added node_id to headers

--- waggle/beehive.py
+++ waggle/beehive.py
@@ -14,6 +14,10 @@ class ClientConfig:
             return ClientConfig(**json.load(f))
 
     def __init__(self, **kwargs):
+        """
+        ClientConfig organizes connection and credential information associated
+        to a Beehive server.
+        """
         self.node = kwargs.get('node', None)
         self.host = kwargs.get('host', 'localhost')
         self.port = kwargs.get('port', None)
@@ -91,7 +95,13 @@ class MessageClient:
 
         self.logger.info('publishing {} on {}'.format(body, topic))
 
+        headers = {}
+
+        if self.config.node is not None:
+            headers['node_id'] = self.config.node
+
         properties = pika.BasicProperties(
+            headers=headers,
             delivery_mode=2,
             timestamp=timestamp,
             content_type=content_type,
