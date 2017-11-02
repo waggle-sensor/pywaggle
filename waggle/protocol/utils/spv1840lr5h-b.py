d09562352f74998eab70389ad40a795b1d271c62
Yongho Kim <ironyonghokim@gmail.com>
2017-11-02 17:33:33 -0500

    Removed numpy

--- waggle/protocol/utils/spv1840lr5h-b.py
+++ waggle/protocol/utils/spv1840lr5h-b.py
@@ -3,10 +3,10 @@
 # V_I = (SPV_TSL_RAW - 1.75) / 453.33 - 1.75   # raw voltage of SPV
 # SPV_RAW = (-V_I * 1023.00) / 5.00          # raw integer [0, 1023] of SPV
 
-import numpy as np
+import math
 
 def convert(value):
     value_voltage = value * 5.0 / 1024.0
     raw = value_voltage / 453.3333 - 1.65
-    db = np.log10(raw) * 20.0
+    db = math.log10(raw) * 20.0
     return db, 'dB'
