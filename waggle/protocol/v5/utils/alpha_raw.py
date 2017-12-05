2c10dbcfcf2420e01c9238e68dc87a1be036bdef
sager <park708@purdue.edu>
2017-12-05 14:57:18 -0600

    fix decoder for alpha serial, firmware reading

--- waggle/protocol/v5/utils/alpha_raw.py
+++ waggle/protocol/v5/utils/alpha_raw.py
@@ -1,10 +1,13 @@
 # Conversion for alphasensor
 
-
 def convert(value):
-    raw_f = value['alpha_firmware']
-
-    version = str(raw_f[0]) + '.' + str(raw_f[1])
-    value['alpha_firmware'] = (version, 'No unit')
+    for key in value:
+        if key == 'alpha_firmware':
+            raw_f = value['alpha_firmware']
+            version = str(raw_f[0]) + '.' + str(raw_f[1])
+            value['alpha_firmware'] = (version, 'No unit')
+        elif key == 'alpha_serial':
+            raw_s = value['alpha_serial']
+            value['alpha_serial'] = (raw_s, 'No unit')
 
     return value
