e0434ab7069244afa58ee8dd265a629aec45fbdd
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-12-14 17:29:52 -0600

    formatted chemsense key names

--- waggle/protocol/v5/utils/chemsense.py
+++ waggle/protocol/v5/utils/chemsense.py
@@ -1,38 +1,41 @@
 # Conversion for chemsense
-
 # chemsense version2, no data of IMU is comming from chemsense --> chemsense FW issue
+
+
+def key_unit(k):
+    if 'T' in k:
+        return 'C'
+    if 'P' in k:
+        return 'hPa'
+    return '%RH'
+
+
+def convert_pair(key, val):
+    if 'BAD' in key:
+        return 'id', val, 'mac'
+    if 'SH' in key or 'HD' in key or 'LP' in key or 'AT' in key or 'LT' in key:
+        return key, float(val)/100.0, key_unit(key)
+    if 'SVL' in key or 'SIR' in key or 'SUV' in key:
+        return key, val, 'raw'
+    return key, int(val), 'raw'
+
 
 def convert(value):
     raw_s = value['chemsense_raw']
 
-    textlist = raw_s.split(' ')
-
     chem_dict = {}
 
-    for i in range(len(textlist)):
-        a_data = textlist[i].split('=')
+    for pair in raw_s.split():
+        try:
+            key, val = pair.split('=')
+        except ValueError:
+            continue
 
-        if len(a_data) == 2:
-            key = a_data[0]
-            val = a_data[1]
+        # ignore sequence number
+        if key == 'SQN':
+            continue
 
-            if 'SQN' in key:
-                continue
-            elif 'BAD' in key:
-                chem_dict['chemsense_id'] = (val, 'mac')
-            elif 'SH' in key or 'HD' in key or 'LP' in key or 'AT' in key or 'LT' in key:
-                val = float(val)/100.0
-                if 'T' in key:
-                    chem_dict[key] = (val, 'C')
-                elif 'P' in key:
-                    chem_dict[key] = (val, 'hPa')
-                else:
-                    chem_dict[key] = (val, '%RH')
-            elif 'SVL' in key or 'SIR' in key or 'SUV' in key:
-                chem_dict[key] = (val, 'raw')
-            else:
-                chem_dict[key] = (int(val), 'raw')
+        k, v, u = convert_pair(key, val)
+        chem_dict['chemsense_' + k.lower()] = (v, u)
 
-        if len(chem_dict) == 0:
-            chem_dict['chemMac'] = ('Chemsense not included', '')
     return chem_dict
