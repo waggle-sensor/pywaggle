a80fb573b4f55c24f856ed0892e40317a0da5ad4
sager <park708@purdue.edu>
2018-02-02 16:31:01 -0600

    change conversion equation

--- waggle/protocol/v5/utils/chemsense.py
+++ waggle/protocol/v5/utils/chemsense.py
@@ -17,22 +17,18 @@ def import_data():
     db = dataset.connect()
     xl = xlrd.open_workbook(directory, "rb")
 
-    M = [16.94, 11.54, 13.83, 12.62, 'inf', 'inf', 12.02]
-    # IRR[0], IAQ[1], SO2[2], H2S[3], OZO[4], NO2[5], CMO[6]
-    # RESP, IAQ, SO2, H2S, OZO, NO2, CMO
-
     for i, sheet in enumerate(xl.sheets()):
         for rownum in range(sheet.nrows):
             rowValues = sheet.row_values(rownum)
             if '088' in str(rowValues[0]):
                 xl_data[str(rowValues[1])] = {
-                    'IRR':{'sensitivity': rowValues[9], 'baseline': rowValues[23], 'Mvalue': 16.94},   # IRR = RESP
-                    'IAQ': {'sensitivity': rowValues[10], 'baseline': rowValues[24], 'Mvalue': 11.54},
-                    'SO2': {'sensitivity': rowValues[11], 'baseline': rowValues[25], 'Mvalue': 13.83},
-                    'H2S': {'sensitivity': rowValues[12], 'baseline': rowValues[26], 'Mvalue': 12.62},
-                    'OZO': {'sensitivity': rowValues[13], 'baseline': rowValues[27], 'Mvalue': 'inf'},
-                    'NO2': {'sensitivity': rowValues[14], 'baseline': rowValues[28], 'Mvalue': 'inf'},
-                    'CMO': {'sensitivity': rowValues[15], 'baseline': rowValues[29], 'Mvalue': 12.02}
+                    'IRR':{'sensitivity': rowValues[9], 'baseline40': rowValues[30], 'Mvalue': rowValues[44]},   # IRR = RESP, baseline = Izero@25C
+                    'IAQ': {'sensitivity': rowValues[10], 'baseline40': rowValues[31], 'Mvalue': rowValues[45]},
+                    'SO2': {'sensitivity': rowValues[11], 'baseline40': rowValues[32], 'Mvalue': rowValues[46]},
+                    'H2S': {'sensitivity': rowValues[12], 'baseline40': rowValues[33], 'Mvalue': rowValues[47]},
+                    'OZO': {'sensitivity': rowValues[13], 'baseline40': rowValues[34], 'Mvalue': rowValues[48]},
+                    'NO2': {'sensitivity': rowValues[14], 'baseline40': rowValues[35], 'Mvalue': rowValues[49]},
+                    'CMO': {'sensitivity': rowValues[15], 'baseline40': rowValues[36], 'Mvalue': rowValues[50]}
                 }
 
     return xl_data
@@ -47,28 +43,25 @@ def key_unit(k):
     return '%RH'
 
 
-def chemical_sensor(ky, instant_current):
+def chemical_sensor(ky, IpA):
     global imported_data
-    zero_temp = 25.0
+    Tzero = 40.0
 
     if len(imported_data) == 0:
         imported_data = import_data()
 
     if mid_dict['BAD'] in imported_data:
-        instant_temp = (float(mid_dict['HDT']) + float(mid_dict['SHT']) + float(mid_dict['LPT'])) / 300.0
+        Tavg = (float(mid_dict['AT0']) + float(mid_dict['AT1']) + float(mid_dict['AT2']) + float(mid_dict['AT3']) + float(mid_dict['LTM'])) / 500.0
 
         sensitivity = imported_data[mid_dict['BAD']][ky]['sensitivity']
-        baseline = imported_data[mid_dict['BAD']][ky]['baseline']
-        M_value = imported_data[mid_dict['BAD']][ky]['Mvalue']
+        baseline = imported_data[mid_dict['BAD']][ky]['baseline40']
+        Minv = imported_data[mid_dict['BAD']][ky]['Mvalue']
 
-        if M_value == 'inf':
-            corrected = float(instant_current) - baseline
-        else:
-            corrected = float(instant_current) - baseline * math.exp((instant_temp - zero_temp) / M_value)
-
-        return corrected, 'ppm'
+        InA = IpA/1000.0 - baseline*math.exp((Tavg - Tzero) / Minv)
+        converted = InA / sensitivity
+        return converted, 'ppm'
     else:
-        return instant_current, 'raw'
+        return IpA, 'raw'
 
 
 def convert_pair(key, val):
