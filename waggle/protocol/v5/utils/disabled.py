77970dbcc8dbfb8aae399cdf8d068a2a19cbac82
sager <park708@purdue.edu>
2018-02-13 16:18:13 -0600

    minor fix

--- waggle/protocol/v5/utils/disabled.py
+++ waggle/protocol/v5/utils/disabled.py
@@ -8,7 +8,7 @@ def convert(value):
     for i in range(len(sensor_id)):
         # print(sensor_id[i], type(sensor_id[i]))
         if sensor_id[i]:
-            num = 'disabled sensor id ' + sensor_id[i]
+            num = 'disabled_sensor_id_' + sensor_id[i]
             value[num] = (sensor_id[i], 'disabled')
 
     return value
