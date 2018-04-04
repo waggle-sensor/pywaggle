083d5547c011a052ca78ae0e73542d84eb28598d
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-04-04 15:14:39 -0500

    added tests for correct parsing

--- waggle/protocol/v5/test_encoder_flat_string.py
+++ waggle/protocol/v5/test_encoder_flat_string.py
@@ -3,13 +3,17 @@ from waggle.protocol.v5.decoder import decode_frame
 from waggle.protocol.v5.encoder import encode_frame_from_flat_string
 
 
+def almostzero(a, b):
+    pass
+
+
 class WaggleProtocolTestUnit(unittest.TestCase):
 
     def test_empty(self):
         self.assertEqual(decode_frame(encode_frame_from_flat_string('')), {})
 
     def test_encode(self):
-        data = '''
+        text = '''
         nc_load_1 0.74
         nc_load_5 0.53
         nc_load_10 0.45
@@ -22,9 +26,24 @@ class WaggleProtocolTestUnit(unittest.TestCase):
         net_broadband_tx 654321
         '''
 
-        encoded_data = encode_frame_from_flat_string(data, True)
+        encoded_data = encode_frame_from_flat_string(text, True)
         decoded_data = decode_frame(encoded_data)
-        print(decoded_data)
+
+        values = {}
+
+        for sensor_values in decoded_data.values():
+            values.update(sensor_values)
+
+        self.assertAlmostEqual(values['nc_load_1'], 0.74)
+        self.assertAlmostEqual(values['nc_load_5'], 0.53)
+        self.assertAlmostEqual(values['nc_load_10'], 0.45)
+        self.assertAlmostEqual(values['nc_ram_total'], 8046136)
+        self.assertAlmostEqual(values['nc_ram_free'], 291328)
+        self.assertAlmostEqual(values['net_broadband_rx'], 123456)
+        self.assertAlmostEqual(values['net_broadband_tx'], 654321)
+
+        self.assertEqual(values['wagman_ver_git'], '1ef3')
+        self.assertEqual(values['nc_boot_id'], '12345678901234567890123456789012')
 
 
 if __name__ == '__main__':
