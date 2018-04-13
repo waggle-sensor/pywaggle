893c2411a42da3f3bc498320d6b6348fa39fa4d1
Yongho Kim <ironyonghokim@gmail.com>
2018-04-13 15:49:14 -0500

    Reduced resolution by 5

--- waggle/protocol/v5/waggleprotocol_spec.py
+++ waggle/protocol/v5/waggleprotocol_spec.py
@@ -1195,12 +1195,12 @@ waggleprotocol_spec = '''
     length: 1
     format: uint
   - name: image_histogram_r
-    length: 85
+    length: 51
     format: byte
   - name: image_histogram_g
-    length: 85
+    length: 51
     format: byte
   - name: image_histogram_b
-    length: 85
+    length: 51
     format: byte
 '''
