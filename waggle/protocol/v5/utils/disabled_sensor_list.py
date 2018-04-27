d50498ee6687431934107b6051ea5a3d7ac5083a
spark <park708@purdue.edu>
2018-04-27 10:53:50 -0500

    Added new sensor: disabled sensor list

--- waggle/protocol/v5/utils/disabled_sensor_list.py
+++ waggle/protocol/v5/utils/disabled_sensor_list.py
@@ -3,7 +3,7 @@
 def convert(value):
     raw_s = value['disabled_sensor']
 
-    sensor_id = raw_s.strip().split('ab')
+    sensor_id = raw_s.strip().split(',')
     value = {}
     for i in range(len(sensor_id)):
         # print(sensor_id[i], type(sensor_id[i]))
