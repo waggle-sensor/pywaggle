908d4d0c8817603a95c845398049635f768f14fc
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-06-22 10:18:56 -0500

    adding unit tests

--- waggle/protocol/v0/__init__.py
+++ waggle/protocol/v0/__init__.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from waggle.checksum import crc8
 from binascii import crc_hqx as crc16
 from binascii import crc32
+import unittest
 
 
 def get_timestamp_or_now(obj):
@@ -12,11 +13,11 @@ def get_timestamp_or_now(obj):
 
 
 def write_uint(w, n, x):
-    w.write(x.to_bytes(n, byteorder='big', signed=False))
+    w.write(x.to_bytes(n, 'big', signed=False))
 
 
 def read_uint(r, n):
-    return int.from_bytes(r.read(n), byteorder='big', signed=False)
+    return int.from_bytes(r.read(n), 'big', signed=False)
 
 
 def write_sensorgram(w, sensorgram):
@@ -26,16 +27,6 @@ def write_sensorgram(w, sensorgram):
     write_uint(w, 1, sensorgram['parameter_id'])
     write_uint(w, 4, get_timestamp_or_now(sensorgram))
     w.write(sensorgram['body'])
-
-
-def pack_sensorgram(sensorgram):
-    w = BytesIO()
-    write_sensorgram(w, sensorgram)
-    return w.getvalue()
-
-
-def unpack_sensorgram(b):
-    return read_sensorgram(BytesIO(b))
 
 
 def read_sensorgram(r):
@@ -54,6 +45,16 @@ def read_sensorgram(r):
     return sensorgram
 
 
+def pack_sensorgram(sensorgram):
+    w = BytesIO()
+    write_sensorgram(w, sensorgram)
+    return w.getvalue()
+
+
+def unpack_sensorgram(b):
+    return read_sensorgram(BytesIO(b))
+
+
 def write_datagram(w, datagram):
     write_uint(w, 1, 0xaa)
     write_uint(w, 3, len(datagram['body']))
@@ -62,11 +63,11 @@ def write_datagram(w, datagram):
     write_uint(w, 2, datagram.get('packet_seq', 0))
     write_uint(w, 1, datagram.get('packet_type', 0))
     write_uint(w, 2, datagram['plugin_id'])
-    write_uint(w, 1, datagram['plugin_major_version'])
-    write_uint(w, 1, datagram['plugin_minor_version'])
-    write_uint(w, 1, datagram['plugin_patch_version'])
-    write_uint(w, 1, datagram['plugin_instance'])
-    write_uint(w, 2, datagram['plugin_run_id'])
+    write_uint(w, 1, datagram.get('plugin_major_version', 0))
+    write_uint(w, 1, datagram.get('plugin_minor_version', 0))
+    write_uint(w, 1, datagram.get('plugin_patch_version', 0))
+    write_uint(w, 1, datagram.get('plugin_instance', 0))
+    write_uint(w, 2, datagram.get('plugin_run_id', 0))
     w.write(datagram['body'])
     write_uint(w, 1, crc8(datagram['body']))
     write_uint(w, 1, 0x55)
@@ -97,6 +98,16 @@ def read_datagram(r):
     return datagram
 
 
+def pack_datagram(datagram):
+    w = BytesIO()
+    write_datagram(w, datagram)
+    return w.getvalue()
+
+
+def unpack_datagram(b):
+    return read_datagram(BytesIO(b))
+
+
 def write_waggle_packet(w, packet):
     assert len(packet['sender_id']) == 16
     assert len(packet['receiver_id']) == 16
@@ -122,9 +133,55 @@ def write_waggle_packet(w, packet):
     write_uint(w, 4, crc32(packet['body']))
 
 
+
+class TestProtocol(unittest.TestCase):
+
+    def test_sensorgram_pack_unpack(self):
+        cases = [
+            {
+                'sensor_id': 1,
+                'parameter_id': 2,
+                'body': b'123',
+            },
+            {
+                'sensor_id': 2,
+                'parameter_id': 4,
+                'body': b'aaaaaaaaax',
+            },
+            {
+                'sensor_id': 2,
+                'sensor_instance': 7,
+                'parameter_id': 4,
+                'body': b'abc',
+            },
+            {
+                'timestamp': 1234567,
+                'sensor_id': 2,
+                'parameter_id': 4,
+                'body': b'ABC',
+            },
+        ]
+
+        for c in cases:
+            r = unpack_sensorgram(pack_sensorgram(c))
+
+            for k in c.keys():
+                self.assertEqual(c[k], r[k])
+
+    def test_datagram_pack_unpack(self):
+        cases = [
+            {
+                'plugin_id': 1,
+                'body': b'123',
+            },
+        ]
+
+        for c in cases:
+            r = unpack_datagram(pack_datagram(c))
+
+            for k in c.keys():
+                self.assertEqual(c[k], r[k])
+
+
 if __name__ == '__main__':
-    print(unpack_sensorgram(pack_sensorgram({
-        'sensor_id': 1,
-        'parameter_id': 2,
-        'body': b'123',
-    })))
+    unittest.main()
