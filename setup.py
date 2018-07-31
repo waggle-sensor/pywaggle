f9d454b15b9c04332f43c234d8f4842e31842776
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-07-31 09:35:52 -0500

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='waggle',
-    version='0.23.14',
+    version='0.24.0',
     description='Python Waggle Module',
     url='https://github.com/waggle-sensor/pywaggle',
     install_requires=[
