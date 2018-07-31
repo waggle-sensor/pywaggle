844920572f5127c9c42887c6e1e030a51f9e2f8a
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-07-31 16:16:26 -0500

    updating docs

--- waggle/plugin/examples/basic-publish.py
+++ waggle/plugin/examples/basic-publish.py
@@ -5,13 +5,13 @@ import time
 plugin = waggle.plugin.PrintPlugin()
 
 while True:
-  # Add our three measurements to the batch.
-  plugin.add_measurement({'sensor_id': 1, 'parameter_id': 0, 'value': 100})
-  plugin.add_measurement({'sensor_id': 1, 'parameter_id': 1, 'value': 32.1})
-  plugin.add_measurement({'sensor_id': 2, 'parameter_id': 0, 'value': b'blob of data'})
+    # Add our three measurements to the batch.
+    plugin.add_measurement({'sensor_id': 1, 'parameter_id': 0, 'value': 100})
+    plugin.add_measurement({'sensor_id': 1, 'parameter_id': 1, 'value': 32.1})
+    plugin.add_measurement({'sensor_id': 2, 'parameter_id': 0, 'value': b'blob of data'})
 
-  # Publish the batch.
-  plugin.publish_measurements()
+    # Publish the batch.
+    plugin.publish_measurements()
 
-  # Wait five seconds before repeating.
-  time.sleep(5)
+    # Wait five seconds before repeating.
+    time.sleep(5)
--- waggle/protocol/examples/basic-pack-and-unpack.py
+++ waggle/protocol/examples/basic-pack-and-unpack.py
@@ -3,14 +3,14 @@ import waggle.protocol
 # We'll make up some test sensor values to pack. Noticed the variety of value
 # types which are supported.
 sensorgrams = [
-  {'sensor_id': 1, 'parameter_id': 0, 'value': 10},
-  {'sensor_id': 2, 'parameter_id': 0, 'value': 22.1},
-  {'sensor_id': 3, 'parameter_id': 0, 'value': b'blob of bytes'},
-  {'sensor_id': 3, 'parameter_id': 1, 'value': 'string'},
-  {'sensor_id': 4, 'parameter_id': 0, 'value': True, 'sensor_instance': 0},
-  {'sensor_id': 4, 'parameter_id': 1, 'value': False, 'sensor_instance': 0},
-  {'sensor_id': 4, 'parameter_id': 2, 'value': None, 'sensor_instance': 1},
-  {'sensor_id': 4, 'parameter_id': 2, 'value': [1.1, 2.2, 3.3]},
+    {'sensor_id': 1, 'parameter_id': 0, 'value': 10},
+    {'sensor_id': 2, 'parameter_id': 0, 'value': 22.1},
+    {'sensor_id': 3, 'parameter_id': 0, 'value': b'blob of bytes'},
+    {'sensor_id': 3, 'parameter_id': 1, 'value': 'string'},
+    {'sensor_id': 4, 'parameter_id': 0, 'value': True, 'sensor_instance': 0},
+    {'sensor_id': 4, 'parameter_id': 1, 'value': False, 'sensor_instance': 0},
+    {'sensor_id': 4, 'parameter_id': 2, 'value': None, 'sensor_instance': 1},
+    {'sensor_id': 4, 'parameter_id': 2, 'value': [1.1, 2.2, 3.3]},
 ]
 
 # First, we'll pack some data of many different types.
