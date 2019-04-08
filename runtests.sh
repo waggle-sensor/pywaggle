0aa9416dad674d0d0aaf97d15c6192a5181472b0
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-04-08 12:43:48 -0500

    tagged shell script

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+# This file is part of the Waggle Platform.  Please see the file
+# LICENSE.waggle.txt for the legal details of the copyright and software
+# license.  For more details on the Waggle project, visit:
+#          http://www.wa8.gl
 
 python3 -m unittest
