3279c47c53e57accbd8c36a33c851aaa35d907e5
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-11-08 10:26:14 -0600

    working on wagman client + logger

--- waggle/protocol/develop/demo.py
+++ waggle/protocol/develop/demo.py
@@ -4,41 +4,62 @@ from base64 import b64encode, b64decode
 import binascii
 import sys
 import time
+import queue
+from threading import Thread
 
-with Serial(sys.argv[1], 9600, timeout=3) as ser:
-    while True:
-        sg = {
-            'timestamp': int(time.time()),
-            'id': 2222,
-            'inst': 33,
-            'sub_id': 44,
-            'source_id': 5555,
-            'source_inst': 66,
-            'value': [7, 88, 9999],
-        }
-        encoded = b64encode(pack_sensorgram(sg))
 
-        print('>>> sending test sensorgram')
-        print(sg)
-        print(encoded.decode())
-        ser.write(encoded)
-        ser.write(b'\n')
-        print()
+# handle response returns
+resp = queue.Queue(1)
 
-        print('>>> printing output')
 
-        while True:
-            line = ser.readline()
-            if len(line) == 0:
-                break
-            print(line.strip().decode())
+def hello():
+    for i in range(100):
+        print(i)
+        try:
+            resp.put(i, timeout=1)
+        except queue.Full:
+            print('resp timed out')
 
-            try:
-                data = b64decode(line.strip())
-                sg = unpack_sensorgram(data)
-                print('sensorgram from device')
-                print(sg)
-            except (binascii.Error, IndexError):
-                pass
 
-        print()
+Thread(target=hello).start()
+
+while True:
+    time.sleep(3)
+
+# with Serial(sys.argv[1], 9600, timeout=3) as ser:
+#     while True:
+#         sg = {
+#             'timestamp': int(time.time()),
+#             'id': 2222,
+#             'inst': 33,
+#             'sub_id': 44,
+#             'source_id': 5555,
+#             'source_inst': 66,
+#             'value': [7, 88, 9999],
+#         }
+#         encoded = b64encode(pack_sensorgram(sg))
+
+#         print('>>> sending test sensorgram')
+#         print(sg)
+#         print(encoded.decode())
+#         ser.write(encoded)
+#         ser.write(b'\n')
+#         print()
+
+#         print('>>> printing output')
+
+#         while True:
+#             line = ser.readline()
+#             if len(line) == 0:
+#                 break
+#             print(line.strip().decode())
+
+#             try:
+#                 data = b64decode(line.strip())
+#                 sg = unpack_sensorgram(data)
+#                 print('sensorgram from device')
+#                 print(sg)
+#             except (binascii.Error, IndexError):
+#                 pass
+
+#         print()
