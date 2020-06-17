0a27fe197fd0d0f8fc777e5ec478672351bcbf54
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-06-17 13:50:03 -0500

    adding simple environment check

---
+++ test_plugin.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+# BUG This test relies on the internal detail that Plugin gets it's
+# env config before connecting to RabbitMQ. This is bad!
+#
+# What this leads to is expecting the config step to pass and then
+# the connection to fail, hence the except socket.gaierror.
+test_plugin_env() {
+    # check for ennvironmental dependencies
+    WAGGLE_PLUGIN_ID=1 \
+    WAGGLE_PLUGIN_VERSION=1.2.3 \
+    WAGGLE_NODE_ID=0000000000000000 \
+    WAGGLE_SUB_ID=0000000000000000 \
+    WAGGLE_PLUGIN_USERNAME=worker \
+    WAGGLE_PLUGIN_PASSWORD=worker \
+    python3 -c '
+from waggle.plugin import Plugin
+import socket
+
+try:
+    Plugin()
+except socket.gaierror:
+    pass
+'
+}
+
+test_plugin_env
