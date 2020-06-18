be6ce3328f2c32be99ee81b38e1815c544fe1ab1
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-06-18 10:14:40 -0500

    allow test_docs to fail if any code block fails

--- test_docs.py
+++ test_docs.py
@@ -2,11 +2,14 @@ from pathlib import Path
 import re
 import subprocess
 import logging
+import sys
 
 
 def run_python_code_block(code):
     return subprocess.check_output(['python3', '-s'], input=code.encode())
 
+
+exitcode = 0
 
 for path in Path('.').glob('**/*.md'):
     text = path.read_text()
@@ -15,5 +18,8 @@ for path in Path('.').glob('**/*.md'):
             print(f'running {path}')
             run_python_code_block(code)
         except Exception as exc:
+            exitcode = 1
             logging.exception(
                 f'code block failed in {path}\n\n```python{code}```\n\n')
+
+sys.exit(exitcode)
