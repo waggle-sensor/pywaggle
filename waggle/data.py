927b9eaedca0d7d5d712a9fa88c46693fcced4a3
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-08-27 10:54:01 -0500

    started test suite and made data driven

--- waggle/data.py
+++ waggle/data.py
@@ -2,10 +2,13 @@ import numpy as np
 from urllib.request import urlopen
 from contextlib import contextmanager
 from threading import Thread
-from queue import Queue
+from queue import Queue, Empty
 import cv2
 import time
 import os
+import socket
+from pathlib import Path
+import json
 
 
 class ImageHandler:
@@ -16,20 +19,18 @@ class ImageHandler:
     def close(self):
         pass
 
-    def get(self):
-        with urlopen(self.url) as f:
-            data = f.read()
-            ts = time.time_ns()
-            arr = np.frombuffer(data, np.uint8)
-            return ts, cv2.imdecode(arr, cv2.IMREAD_COLOR)
+    def get(self, timeout=None):
+        try:
+            with urlopen(self.url, timeout=timeout) as f:
+                data = f.read()
+                ts = time.time_ns()
+                arr = np.frombuffer(data, np.uint8)
+                return ts, cv2.imdecode(arr, cv2.IMREAD_COLOR)
+        except socket.timeout:
+            raise TimeoutError('get timed out')
 
 
-def video_worker(url, out):
-    cap = cv2.VideoCapture(url)
-
-    if not cap.isOpened():
-        raise RuntimeError(f'could not open camera at "{url}".')
-
+def video_worker(cap, out):
     while True:
         ok, img = cap.read()
         if ok:
@@ -43,33 +44,61 @@ def video_worker(url, out):
 class VideoHandler:
 
     def __init__(self, url):
+        cap = cv2.VideoCapture(url)
+
+        if not cap.isOpened():
+            raise RuntimeError(f'could not open camera at "{url}".')
+
         self.queue = Queue()
+
         self.worker = Thread(target=video_worker, args=(
-            url, self.queue), daemon=True)
+            cap, self.queue), daemon=True)
         self.worker.start()
 
     def close(self):
         pass
 
-    def get(self):
-        return self.queue.get()
+    def get(self, timeout=None):
+        try:
+            return self.queue.get(timeout=timeout)
+        except Empty:
+            raise TimeoutError('get timed out')
 
 
-def get_camera_image_endpoint(name, **kwargs):
-    return os.environ['WAGGLE_SES_CAMERA_IMAGE_ENDPOINT']
+with Path('data-config.json').open() as f:
+    config = json.load(f)
 
 
-def get_camera_video_endpoint(name, **kwargs):
-    return os.environ['WAGGLE_SES_CAMERA_VIDEO_ENDPOINT']
+def dict_is_subset(a, b):
+    return all(k in b and a[k] == b[k] for k in a.keys())
 
 
-table = {
-    'camera/image': (get_camera_image_endpoint, ImageHandler),
-    'camera/video': (get_camera_video_endpoint, VideoHandler),
+def find_all_matches(query):
+    return [c for c in config if dict_is_subset(query, c['match'])]
+
+
+def find_match(query):
+    matches = find_all_matches(query)
+    if len(matches) == 0:
+        raise RuntimeError('no matches found')
+    if len(matches) > 1:
+        raise RuntimeError('multiple devices found')
+    return matches[0]
+
+
+handlers = {
+    'image': ImageHandler,
+    'video': VideoHandler,
 }
 
 
 @contextmanager
-def open_data_source(name, **kwargs):
-    get_endpoint, handler = table[name]
-    yield handler(get_endpoint(name, **kwargs))
+def open_data_source(query):
+    match = find_match(query)
+    handler = handlers[match['handler']['type']]
+    yield handler(**match['handler']['args'])
+
+
+if __name__ == '__main__':
+    with open_data_source({'type': 'camera/image', 'orientation': 'bottom'}) as s:
+        print(s.get())
