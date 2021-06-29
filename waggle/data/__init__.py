f771350b8f26bb280a49e0edb1c5dfd8cc4ddda8
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-06-29 16:37:57 -0500

    removed stale comments

--- waggle/data/__init__.py
+++ waggle/data/__init__.py
@@ -1,16 +1,2 @@
-# from .audio import Microphone, AudioFolder
-# from .vision import Camera, ImageFolder
-# from .measurements import MeasurementsFile
-
 # Maintaining backwards compatibility for now.
 from .data_shim import open_data_source
-
-# USE ENV VAR TO DISTINGUISH DEV / TEST / PROD???
-
-# take common, easy resources and make easy to access...
-# alternatively, abstract out the data flow...
-
-# some ideas... make different sizes to ensure user handles this??
-# images = waggle.data.test_images()
-# video = waggle.data.test_video()
-# audio = waggle.data.test_audio()
