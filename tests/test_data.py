cb90edaa5e6e0479133a83c75278e66a5052b09d
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-08-09 09:57:47 -0500

    added test for get_timestamp to check that it works across Python versions

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,6 @@
 import unittest
 from waggle.data.vision import RGB, BGR
+from waggle.data.timestamp import get_timestamp
 import numpy as np
 
 class TestData(unittest.TestCase):
@@ -9,6 +10,10 @@ class TestData(unittest.TestCase):
             data = np.random.randint(0, 255, (100, 120, 3), dtype=np.uint8)
             data2 = fmt.format_to_cv2(fmt.cv2_to_format(data))
             self.assertTrue(np.all(np.isclose(data, data2, 1.0)), f"checking format {fmt}")
+    
+    def test_get_timestamp(self):
+        ts = get_timestamp()
+        self.assertIsInstance(ts, int)
 
 if __name__ == '__main__':
     unittest.main()
