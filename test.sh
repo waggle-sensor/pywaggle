74e6c55c5a50b56254f9743fe7be31983b5b2b18
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-01-05 15:38:43 -0600

    reorganized package so we can publish to pypi (#27)

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# run tests on local waggle/ directory
-python3 -m unittest discover -s tests
-
-# to run tests on installed package, use:
-# (cd tests && python3 -m unittest discover)
