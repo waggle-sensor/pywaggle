a6f8a4b6264ee1cdb91368cddae1284c7352c40d
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-07-11 19:56:24 -0500

    consolidated integration tests with existing test suite

---
+++ Makefile
@@ -0,0 +1,13 @@
+all:
+
+.PHONY: test
+test:
+	PYTHONPATH=src python3 -m unittest discover tests
+
+.PHONY: svc-up
+svc-up:
+	docker-compose up -d
+
+.PHONY: svc-down
+svc-down:
+	docker-compose down
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -4,6 +4,9 @@ import json
 from tempfile import TemporaryDirectory
 import time
 from datetime import datetime
+import os
+import pika
+import subprocess
 
 from waggle.plugin import Plugin, PluginConfig, Uploader, get_timestamp
 import wagglemsg
@@ -156,6 +159,7 @@ class TestPlugin(unittest.TestCase):
             msg = wagglemsg.load(item.body)
             self.assertEqual(msg.name, "dur")
 
+
 class TestUploader(unittest.TestCase):
     
     def test_upload_file(self):
@@ -175,6 +179,68 @@ class TestUploader(unittest.TestCase):
             self.assertIn('timestamp', meta)
             self.assertIn('shasum', meta)
             self.assertEqual(meta['labels']['filename'], upload_path.name)
+
+
+
+def rabbitmq_available():
+    try:
+        subprocess.check_output(["docker-compose", "exec", "rabbitmq", "true"])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def get_admin_connection():
+    params = pika.ConnectionParameters(credentials=pika.PlainCredentials("admin", "admin"))
+    return pika.BlockingConnection(params)
+
+
+@unittest.skipIf(not rabbitmq_available(), "rabbitmq unavailable")
+class TestPluginWithRabbitMQ(unittest.TestCase):
+
+    def setUp(self):
+        os.environ["WAGGLE_PLUGIN_USERNAME"] = "plugin"
+        os.environ["WAGGLE_PLUGIN_PASSWORD"] = "plugin"
+        os.environ["WAGGLE_PLUGIN_HOST"] = "127.0.0.1"
+        os.environ["WAGGLE_PLUGIN_PORT"] = "5672"
+
+        with get_admin_connection() as conn, conn.channel() as ch:
+            ch.queue_purge("to-validator")
+    
+    def test_publish(self):
+        now = time.time_ns()
+
+        with Plugin() as publisher:
+            publisher.publish("test", 123, meta={"sensor": "bme680"}, timestamp=now)
+
+        with get_admin_connection() as conn, conn.channel() as ch:
+            _, _, body = ch.basic_get("to-validator", auto_ack=True)
+            msg = wagglemsg.load(body)
+        
+        self.assertEqual(msg, wagglemsg.Message(
+            name="test",
+            value=123,
+            meta={"sensor": "bme680"},
+            timestamp=now,
+        ))
+
+    def test_subscribe(self):
+        msg = wagglemsg.Message(
+            name="test",
+            value=123,
+            meta={"sensor": "bme680"},
+            timestamp=time.time_ns(),
+        )
+
+        with Plugin() as subscriber:
+            subscriber.subscribe("test")
+            time.sleep(1)
+
+            with get_admin_connection() as conn, conn.channel() as ch:
+                ch.basic_publish("data.topic", "test", wagglemsg.dump(msg))
+
+            msg2 = subscriber.get(1)
+            self.assertEqual(msg, msg2)
 
 
 if __name__ == '__main__':
